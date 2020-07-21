7b79775b92e283d9e248a362955081a5deb36d4b
Dean Huang <huangheqing426@gmail.com>
2020-07-20 19:31:33 -0700

    Added firebase services to the project

--- sushi_go/lib/game.dart
+++ sushi_go/lib/game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Game extends StatelessWidget {
   String user_name;
@@ -15,7 +16,33 @@ class Game extends StatelessWidget {
       appBar: AppBar(
         title: Text('Welcome to Sushi go' + user_name),
       ),
-      body: Center(),
+      body: Center(child: new BookList()),
+    );
+  }
+}
+
+class BookList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('sushi-go').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return new Text('Loading...');
+          default:
+            return new ListView(
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
+                return new ListTile(
+                  title: new Text(document['room_name']),
+                  subtitle: new Text(document['owner']),
+                );
+              }).toList(),
+            );
+        }
+      },
     );
   }
 }
